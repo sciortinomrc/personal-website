90471f1f684989ee1f09569dc46722dc0a3dc956
Marco <sciortino.mrc@gmail.com>
2019-02-08 17:17:38 +0000

    description edit

--- src/Main/PersonalData.js
+++ src/Main/PersonalData.js
@@ -8,7 +8,7 @@ const PersonalData=()=>{
 				<img src={photo} id="pic" alt="Me"/>
 				<h1 className="text-white">{"Marco Sciortino"}</h1>
 				<h2 className="text-custom-2"><span className="text-custom-1"> Full-Stack </span> Web Developer </h2>
-				<p><span className="text-custom-2 text-20">--UK-based Italian, Problem solver, Algorithm enthusiast, Good at making things happen--</span>  Open to contract opportunities - </p>
+				<p><span className="text-custom-2 text-20">--UK-based Italian, Problem solver, Algorithm enthusiast, Good at making things happen--</span>  Open to any type of opportunity - </p>
 			<p id="resume"><a href={PDF} target="_blank" rel="noopener noreferrer">Download Resume</a></p>			</div>
 
 		</div>
