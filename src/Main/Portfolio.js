4333e3c5f7f5371e5beee17b67f7650cd318f36b
Marco Sciortino <marco.sciortino@meterian.com>
2020-04-08 21:50:21 +0100

    changed url on portoliio

--- src/Main/Portfolio.js
+++ src/Main/Portfolio.js
@@ -10,34 +10,34 @@ const Portfolio=()=>{
 		<div id="portfolio" class="text-center" >
 			<h2 className="text-custom-2">My Work</h2>
 				<div class="d-flex flex-wrap justify-content-center">
-					<a href="https://pagesify.herokuapp.com" class="m-4" target="_blank"  rel="noopener noreferrer" >
+					<a href="https://pagesify.mrcdev.co.uk" class="m-4" target="_blank"  rel="noopener noreferrer" >
 						<div className="description">
 							<h4>Pagesify - Facebook pages showcase</h4>
 							<ul>
 								<li>- Front end: React - Redux - CSS</li>
-								<li>- Back end: ExpressJS - PostgreSQL</li>
+								<li>- Back end: ExpressJS - MySQL - AI</li>
 							</ul>
 						</div>
 						<div>
 							<img src={pagesify} alt="Pagesify"  />
 						</div>		
 					</a>
-					<a href="https://my-cool-calendar.netlify.com/" class="m-4" target="_blank"  rel="noopener noreferrer">
+					<a href="https://mrcdev.co.uk/apps/my_cool_calendar/" class="m-4" target="_blank"  rel="noopener noreferrer">
 						<div className="description">
 							<h4>Fake calendar for reminders</h4>
 							<ul>
-								<li>- React/Redux, CSS3</li>
+								<li>- React - Redux - CSS3</li>
 							</ul>
 						</div>
 						<div>
 							<img src={calendar} alt="My-Cool-Calendar"  />
 						</div>
 					</a>
-					<a href="https://sciortinomrc.github.io/matching-game/" class="m-4" target="_blank"  rel="noopener noreferrer">
+					<a href="https://mrcdev.co.uk/apps/matching_game/" class="m-4" target="_blank"  rel="noopener noreferrer">
 						<div className="description">
 							<h4>Matching game</h4>
 							<ul>
-								<li>- HTML5, CSS, Vanilla Javascript</li>
+								<li>- HTML5 - CSS - Vanilla Javascript</li>
 							</ul>
 						</div>
 						<div>
